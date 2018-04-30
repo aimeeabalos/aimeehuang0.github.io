8a548dffdf2792be8d9cd44492834b1e25fc4d27
aimeehuang0 <abalos.aimee@gmail.com>
2018-04-30 19:00:03 -0400

    change first page

--- js/main.js
+++ js/main.js
@@ -10,24 +10,24 @@ var controller = new ScrollMagic.Controller({
 });
 
 /*only for logo*/
-var tweenForAimee = new TimelineMax()
-  .add([
-      TweenMax.from(".scroll-name div", 2, {
-        y: 0,
-        ease: Back.ease
-      }),
-      TweenMax.to(".scroll-name div", 2, {
-        y: '-500vh',
-        ease: Back.ease
-      })
-   ]);
+// var tweenForAimee = new TimelineMax()
+//   .add([
+//       TweenMax.from(".scroll-name div", 2, {
+//         y: 0,
+//         ease: Back.ease
+//       }),
+//       TweenMax.to(".scroll-name div", 2, {
+//         y: '-500vh',
+//         ease: Back.ease
+//       })
+//    ]);
 
-var sceneHi = new ScrollMagic.Scene({
-    duration: '100%'
-  })
-  .setTween(tweenForAimee)
-  .setPin(".scroll-name")
-  .addTo(controller);
+// var sceneHi = new ScrollMagic.Scene({
+//     duration: '100%'
+//   })
+//   .setTween(tweenForAimee)
+//   .setPin(".scroll-name")
+//   .addTo(controller);
 
 
 var scencework = new ScrollMagic.Scene({
@@ -84,3 +84,60 @@ $(document).ready(function () {
     // Fragment doesn't exist
   }
 });
+
+var TxtType = function(el, toRotate, period) {
+		        this.toRotate = toRotate ;
+		        this.el = el;
+		        this.loopNum = 0;
+		        this.period = parseInt(period, 10) || 2000;
+		        this.txt = '';
+		        this.tick();
+		        this.isDeleting = false;
+		    };
+
+		    TxtType.prototype.tick = function() {
+		        var i = this.loopNum % this.toRotate.length;
+		        var fullTxt = this.toRotate[i];
+
+		        if (this.isDeleting) {
+		        this.txt = fullTxt.substring(0, this.txt.length - 1);
+		        } else {
+		        this.txt = fullTxt.substring(0, this.txt.length + 1);
+		        }
+
+		        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+		        var that = this;
+		        var delta = 200 - Math.random() * 100;
+
+		        if (this.isDeleting) { delta /= 2; }
+
+		        if (!this.isDeleting && this.txt === fullTxt) {
+		        delta = this.period;
+		        this.isDeleting = true;
+		        } else if (this.isDeleting && this.txt === '') {
+		        this.isDeleting = false;
+		        this.loopNum++;
+		        delta = 500;
+		        }
+
+		        setTimeout(function() {
+		        that.tick();
+		        }, delta);
+		    };
+
+		    window.onload = function() {
+		        var elements = document.getElementsByClassName('typewrite');
+		        for (var i=0; i<elements.length; i++) {
+		            var toRotate = elements[i].getAttribute('data-type');
+		            var period = elements[i].getAttribute('data-period');
+		            if (toRotate) {
+		              new TxtType(elements[i], JSON.parse(toRotate), period);
+		            }
+		        }
+		        // INJECT CSS
+		        var css = document.createElement("style");
+		        css.type = "text/css";
+		        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+		        document.body.appendChild(css);
+		    };
